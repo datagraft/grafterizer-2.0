94a22c960e8c6e035c8c923458d9f970360197af
Bjørn Marius von Zernichow <BjornMarius.vonZernichow@sintef.no>
2017-07-31 11:23:42 +0200

    Initial table implementation

--- src/app/tabular-transformation/handsontable/handsontable.component.ts
+++ src/app/tabular-transformation/handsontable/handsontable.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var Handsontable: any;
+
 @Component({
   selector: 'handsontable',
   templateUrl: './handsontable.component.html',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HandsontableComponent implements OnInit {
 
-  constructor() { }
+  // handsontable instance
+  private hot: any;
+  private data: Array<any>;
+  private container: any;
+  private settings: Object;
+
+  constructor() {
+    // init table
+    this.data = [];
+    for (let i = 0; i <= 20; i++) {
+      this.data.push(["-", "-", "-", "-", "-"]);
+    }
+   }
 
   ngOnInit() {
-  }
+    this.container = document.getElementById('handsontable');
+    this.settings = {
+      data: this.data,
+      rowHeaders: true,
+      colHeaders: true,
+      columnSorting: false,
+      viewportColumnRenderingOffset: 40,
+      contextMenu: {
+        callback: (key, options) => {
+          if (key === 'row_above' || 'row_below' || 'remove_col' || 'remove_row' || 'col_left' || 'col_right' || 'undo' || 'redo' || 'zero') {
+            console.log(key);
 
+          };
+          if (key === "zero") {
+            console.log(key);
+          };
+        },
+        items: {
+          "row_above": {},
+          "row_below": {},
+          "remove_col": {},
+          "remove_row": {},
+          "col_left": {},
+          "col_right": {},
+          "zero": { name: 'Set empty cells to median' },
+          "undo": {},
+          "redo": {}
+        },
+      },
+      height: 540,
+      stretchH: 'all',
+      className: 'htCenter htMiddle',
+      afterSelection: (r, c, r2, c2) => {
+            console.log('OK');        
+      }
+    }
+    this.hot = new Handsontable(this.container, this.settings);
+    }
 }
