0ec3cfce7619438c274152ee8d0b2a693da6d7ed
Bjørn Marius von Zernichow <BjornMarius.vonZernichow@sintef.no>
2017-09-25 15:36:41 +0200

    Generated new pipeline component

---
+++ src/app/tabular-transformation/sidebar/pipeline/pipeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipeline',
+  templateUrl: './pipeline.component.html',
+  styleUrls: ['./pipeline.component.css']
+})
+export class PipelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
