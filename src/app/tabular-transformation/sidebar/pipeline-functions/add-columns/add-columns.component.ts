9aeaf270df2e35883cac4ba9f03c40ff0fcf6963
SINTEFGRP\bjornv <BjornMarius.vonZernichow@sintef.no>
2018-02-25 19:15:31 +0100

    Implemented pipeline function 'add-columns'

--- src/app/tabular-transformation/sidebar/pipeline-functions/add-columns/add-columns.component.ts
+++ src/app/tabular-transformation/sidebar/pipeline-functions/add-columns/add-columns.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input, Output, OnChanges, SimpleChanges, EventEmitter } from '@angular/core';
-import { CompleterService, CompleterData } from 'ng2-completer';
-import * as transformationDataModel from '../../../../../assets/transformationdatamodel.js';
+import { Component, OnInit } from '@angular/core';
 
+import * as transformationDataModel from '../../../../../assets/transformationdatamodel.js';
+import { Subscription } from 'rxjs/Subscription';
+import { PipelineEventsService } from 'app/tabular-transformation/pipeline-events.service';
+import { TransformationService } from 'app/transformation.service';
 
 @Component({
   selector: 'add-columns',
@@ -9,67 +11,123 @@ import * as transformationDataModel from '../../../../../assets/transformationda
   styleUrls: ['./add-columns.component.css']
 })
 
-export class AddColumnsComponent implements OnInit, OnChanges {
+export class AddColumnsComponent implements OnInit {
 
-  @Input() modalEnabled;
-  @Input() function: any;
-  @Output() emitter = new EventEmitter();
+  modalEnabled = false;
 
-  private addcolumnsmode: String[] = [];
-  private columnsArray: any[];
-  private docstring: String;
+  private currentlySelectedFunctionSubscription: Subscription;
+  private currentlySelectedFunction: any;
 
-  constructor(private completerService: CompleterService) { }
+  private pipelineEventsSubscription: Subscription;
+  private pipelineEvent: any;
+
+  private previewedDataSubscription: Subscription;
+  private previewedDataColumns: any;
+
+  private colname: String = '';
+  private columnsArray: any = [];
+  private docstring: String = '';
+
+  constructor(private pipelineEventsSvc: PipelineEventsService, private transformationSvc: TransformationService) { }
 
   ngOnInit() {
     this.modalEnabled = false;
-    this.initFunction();
+    this.currentlySelectedFunctionSubscription = this.pipelineEventsSvc.currentlySelectedFunction.subscribe((selFunction) => {
+      this.currentlySelectedFunction = selFunction.currentFunction;
+    });
+
+    this.pipelineEventsSubscription = this.pipelineEventsSvc.currentPipelineEvent.subscribe((currentEvent) => {
+      this.pipelineEvent = currentEvent;
+      // In case we clicked edit      
+      if (currentEvent.startEdit && this.currentlySelectedFunction.__type === 'AddColumnsFunction') {
+        this.modalEnabled = true;
+        this.columnsArray = this.currentlySelectedFunction.columnsArray;
+        this.docstring = this.currentlySelectedFunction.docstring;
+      }
+      // In case we clicked to add a new data cleaning step
+      if (currentEvent.createNew && currentEvent.newStepType === 'AddColumnsFunction') {
+        this.modalEnabled = true;
+        var newColSpec = new transformationDataModel.NewColumnSpec(null, null, null, null);
+        this.columnsArray.push(newColSpec);
+      }
+    });
+
+    this.previewedDataSubscription = this.transformationSvc.currentGraftwerkData
+      .subscribe((previewedData) => {
+        if (previewedData[':column-names']) {
+          this.previewedDataColumns = previewedData[':column-names'].map(o => o.substring(1, o.length));
+          console.log(this.previewedDataColumns);
+        }
+      });
   }
 
-  initFunction() {
-    this.docstring = '';
-    this.columnsArray = [new transformationDataModel.NewColumnSpec(null, null, null, null)];
-    this.function = new transformationDataModel.AddColumnsFunction(this.columnsArray, this.docstring);
-    this.addcolumnsmode = ['text'];
-  }
-
-  addColumn() {
-    var newColSpec = new transformationDataModel.NewColumnSpec("", "", "", "");
+  private addColumn() {
+    var newColSpec = new transformationDataModel.NewColumnSpec(null, null, null, null);
     this.columnsArray.push(newColSpec);
   }
 
-  removeColumn(idx: number) {
-    this.columnsArray.splice(idx, 1);
-    this.addcolumnsmode.splice(idx, 1);
+  private removeColumn(id: number) {
+    this.columnsArray.splice(id, 1);
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.function) {
-      if (!this.function) {
-        // console.log('New function');
-      }
-      else {
-        console.log('Edit function');
-        if (this.function.__type == 'AddColumnsFunction') {
-          this.columnsArray = this.function.columnsArray;
-          for (let colVal of this.function.columnsArray) {
-            if (colVal.colValue) { this.addcolumnsmode.push('text'); }
-          }
-          this.docstring = this.function.docstring;
-        }
-      }
+  private accept() {
+    if (this.pipelineEvent.startEdit) {
+      // change currentlySelectedFunction according to the user choices
+      this.createAddColumnsFunction(this.currentlySelectedFunction);
+
+      // notify of change in selected function
+      this.pipelineEventsSvc.changeSelectedFunction({
+        currentFunction: this.currentlySelectedFunction,
+        changedFunction: this.currentlySelectedFunction
+      });
+
+      // notify of that we finished editing
+      this.pipelineEventsSvc.changePipelineEvent({
+        commitEdit: true,
+        preview: true
+      });
     }
-  }
+    else if (this.pipelineEvent.createNew) {
+      // create empty object
+      const newFunction = new transformationDataModel.AddColumnsFunction(this.columnsArray, this.docstring);
 
-  accept() {
-    // console.log(this.function);
-    this.function.columnsArray = this.columnsArray;
-    this.function.docstring = this.docstring;
-    this.emitter.emit(this.function);
-    this.initFunction();
+      // apply user choices to the empty object
+      this.createAddColumnsFunction(newFunction);
+
+      // notify of change in selected function
+      this.pipelineEventsSvc.changeSelectedFunction({
+        currentFunction: this.currentlySelectedFunction,
+        changedFunction: newFunction
+      });
+
+      // notify of that we finished creating
+      this.pipelineEventsSvc.changePipelineEvent({
+        commitCreateNew: true
+      });
+    } else {
+      console.log('ERROR! Something bad happened!');
+    }
+
     this.modalEnabled = false;
+    this.resetModal();
   }
 
-  cancel() { this.modalEnabled = false; }
+  private createAddColumnsFunction(instanceObj): any {
+    instanceObj.columnsArray = this.columnsArray;
+    instanceObj.docstring = this.docstring;
+    instanceObj.isPreviewed = true;
+  }
+
+  // resets the fields of the modal
+  private resetModal() {
+    this.columnsArray = [];
+    this.docstring = 'Add columns';
+  }
+
+  private cancel() {
+    // change event
+    this.modalEnabled = false;
+    this.resetModal();
+  }
 
 }
