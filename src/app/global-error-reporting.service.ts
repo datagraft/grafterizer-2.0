c9691918a68dc6d03c898e272d54760a859e667e
Nikolay Nikolov <nikolay.nikolov@sintef.no>
2018-02-28 16:11:16 +0100

    Added reporting of pipeline errors to the global error reporting service

--- src/app/global-error-reporting.service.ts
+++ src/app/global-error-reporting.service.ts
@@ -8,12 +8,21 @@ export class GlobalErrorReportingService {
   private globalErrorSubject: BehaviorSubject<Array<any>>;
   public globalErrorObs: Observable<any>;
 
+  private previewErrorSubject: BehaviorSubject<Array<any>>;
+  public previewErrorObs: Observable<any>;
+
   constructor() {
     this.globalErrorSubject = new BehaviorSubject<Array<any>>([]);
     this.globalErrorObs = this.globalErrorSubject.asObservable();
+    this.previewErrorSubject = new BehaviorSubject<Array<any>>(undefined);
+    this.previewErrorObs = this.previewErrorSubject.asObservable();
   }
 
   public changeGlobalErrors(errors: Array<any>) {
     this.globalErrorSubject.next(errors);
+  }
+
+  public changePreviewError(error: any) {
+    this.previewErrorSubject.next(error);
   }
 }
