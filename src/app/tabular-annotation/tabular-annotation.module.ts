00d51e8ac4bc46acc6e5f4af3fac07628c17ada6
Unknown <v.cutrona1@gmail.com>
2019-03-25 14:28:31 +0100

    replace clr icons and fix buttons placement

--- src/app/tabular-annotation/tabular-annotation.module.ts
+++ src/app/tabular-annotation/tabular-annotation.module.ts
@@ -1,33 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NglModule } from 'ng-lightning/ng-lightning';
-import { NguiAutoCompleteModule } from '@ngui/auto-complete';
-import { TabularAnnotationComponent } from './tabular-annotation.component';
-import { AnnotationFormComponent } from './annotation-form/annotation-form.component';
-import { ClarityModule } from '@clr/angular';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NglModule} from 'ng-lightning/ng-lightning';
+import {NguiAutoCompleteModule} from '@ngui/auto-complete';
+import {TabularAnnotationComponent} from './tabular-annotation.component';
+import {AnnotationFormComponent} from './annotation-form/annotation-form.component';
+import {ClarityModule} from '@clr/angular';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-        MatDialogModule,
-        MatExpansionModule,
-        MatInputModule,
-        MatTableModule,
-        MatIconModule,
-        MatSelectModule,
-        MatFormFieldModule,
-        MatCheckboxModule,
-        MatSortModule,
-        MatPaginatorModule,
-        MatProgressSpinnerModule,
-        MatAutocompleteModule,
-        MatChipsModule
-       } from '@angular/material';
-import { ConfigComponent } from './config/config.component';
-import { TagInputModule } from 'ngx-chips';
-import { ReconciliationComponent } from './enrichment/reconciliation/reconciliation.component';
-import { AddEntityDialog } from './enrichment/reconciliation/addEntityDialog.component';
-import { ChooseExtensionOrReconciliationDialog} from './chooseExtensionOrReconciliationDialog.component';
-import { ExtensionComponent } from './enrichment/extension/extension.component';
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
+import {ConfigComponent} from './config/config.component';
+import {TagInputModule} from 'ngx-chips';
+import {ReconciliationComponent} from './enrichment/reconciliation/reconciliation.component';
+import {AddEntityDialog} from './enrichment/reconciliation/addEntityDialog.component';
+import {ChooseExtensionOrReconciliationDialog} from './chooseExtensionOrReconciliationDialog.component';
+import {ExtensionComponent} from './enrichment/extension/extension.component';
 import {SharedModule} from './shared/shared.module';
 
 @NgModule({
@@ -53,6 +54,7 @@ import {SharedModule} from './shared/shared.module';
     MatProgressSpinnerModule,
     MatAutocompleteModule,
     MatChipsModule,
+    MatButtonModule,
     SharedModule
   ],
   entryComponents: [AnnotationFormComponent,
